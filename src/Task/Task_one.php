f14c3ca6b9d905abf4c3339ee9400864f739f62e
Mdsujansarkar <sujansarkar786@gmail.com>
2022-01-10 18:14:10 +0600

    Task One complite

---
+++ src/Task/Task_one.php
@@ -0,0 +1,28 @@
+<?php 
+
+class Task_one
+{
+    public $numberList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    public function task_one()
+    {
+        for($i = 0; $i < count($this->numberList); $i++)
+        {
+            if($this->numberList[$i]%3==0)
+            {
+                echo $this->numberList[$i] . "pa\n";
+            }
+
+            if($this->numberList[$i]%5==0)
+            {
+                echo $this->numberList[$i] . "pow\n";
+            }
+            if($this->numberList[$i]%5 ==0 && $this->numberList[$i]%3==0)
+            {
+                echo $this->numberList[$i] . "papow\n";
+            }
+        }
+    }
+}
+
+$ob= new Task_one();
+$ob->task_one();
